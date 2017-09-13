0fe37297f8a874626769ff91928616cd2d60823c
Yomar Miranda <mirandayomar@gmail.com>
2017-09-12 21:36:13 -0500

    Aquí se muestra un mensaje cuando se pulsa sobre cualquiera de los dos divs

---
+++ triggers/js/core.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("#item1").click(function(){
+		alert("Has hecho click en el div #1");
+	})
+
+	$("#item2").click(function(){
+		alert("Has hecho click en el div #2");
+	})
+})
